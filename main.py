cec493f444bb19283c9b6cdc9fce2de4e757b93b
0xpedrojorge <pedmjjorge@gmail.com>
2021-05-11 18:15:41 +0100

    Minor changes

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pyrebase
+import tweepy
 
-config = {
-	"apiKey": "AIzaSyCkrviVAVRis0KfHyHq64kuMCKj-2rOY6M",
-	"authDomain": "watering-from-twitter.firebaseapp.com",
-	"databaseURL": "https://watering-from-twitter-default-rtdb.europe-west1.firebasedatabase.app",
-	"storageBucket": "watering-from-twitter.appspot.com",
-	"serviceAccount": "./watering-from-twitter-firebase-adminsdk-ox1ft-2158d5aed1.json"
-}
+def main():
+
+	firebaseConfig = {
+		"apiKey": "AIzaSyCkrviVAVRis0KfHyHq64kuMCKj-2rOY6M",
+		"authDomain": "watering-from-twitter.firebaseapp.com",
+		"databaseURL": "https://watering-from-twitter-default-rtdb.europe-west1.firebasedatabase.app",
+		"storageBucket": "watering-from-twitter.appspot.com",
+		"serviceAccount": "./watering-from-twitter-firebase-adminsdk-ox1ft-2158d5aed1.json"
+	}
+
+	twitterConfig = {
+		"consumer_key": "",
+		"consumer_secret": "",
+		"access_token": "",
+		"access_token_secret": ""
+	}
 
 
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
+	firebase = pyrebase.initialize_app(firebaseConfig)
+	db = firebase.database()
 
-plants = db.get()
-print(plants.val())
+	plants = db.get()
+	print(plants.val())
+
+if __name__ == "__main__":
+	main()
