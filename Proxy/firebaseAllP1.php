910431930527222851252099aea4de70ba69054a
DavidFernandes96 <36897055+DavidFernandes96@users.noreply.github.com>
2021-05-31 01:34:34 +0100

    Update firebaseAllP1.php

--- Proxy/firebaseAllP1.php
+++ Proxy/firebaseAllP1.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'firebaseLib.php';
 // --- This is your Firebase URL
-$url = 'https://watering-from-twitter-default-rtdb.europe-west1.firebasedatabase.app/';
+$url = '*****';
 // --- Use your token from Firebase here
-$token = 'sTCZTZKC1fwVBAM3GQ6e4UsAwAKg4BGtUTOl4MW3';
+$token = '*****';
 // --- Here is your parameter from the http GET
 $arduino_data_1 = $_GET['arg1'];
 $arduino_data_2 = $_GET['arg2'];
