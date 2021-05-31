35df92a329f0ed257c559850433523a87ada3c13
DavidFernandes96 <36897055+DavidFernandes96@users.noreply.github.com>
2021-05-31 01:45:55 +0100

    Update sketchTrabalho2.ino

--- sketchTrabalho2/sketchTrabalho2.ino
+++ sketchTrabalho2/sketchTrabalho2.ino
@@ -5,8 +5,8 @@
   Wireless credentials
  *
 */
-const char ssid[] = "*****";  // network SSID (name)
-const char pass[] = "*****";            // network password
+const char ssid[] = "*****";                // network SSID (name)
+const char pass[] = "*****";                // network password
 int status = WL_IDLE_STATUS;                // the Wifi radio's status
 
 WiFiClient client;
